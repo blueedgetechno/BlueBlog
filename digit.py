5361c70378b58939b93680f81e49a2a5d5c4772f
blueedgetechno <blueedgetechno@gmail.com>
2020-07-04 14:48:53 +0530

    commit

--- digit.py
+++ digit.py
@@ -1,13 +1,13 @@
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 from numpy import array, argmax
-from tensorflow.keras import backend as K
-K.clear_session()
+# from tensorflow.keras import backend as K
+# K.clear_session()
 
-model = load_model('./projects/keras_mnist.h5')
 class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
 def guess(num):
+    model = load_model('./projects/keras_mnist.h5')
     num = array(num).T
     num = array([num])
     predictions = model.predict(num)
